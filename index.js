54500592543d83cda0fd2b43f96b74934e1aabf3
olasrum <mursalo@gmail.com>
2022-08-14 15:23:40 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-\s]+/g, '');
+  }
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let array = string.split(' ')
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(array[i]))
+      } else {
+        if (exceptions.includes(array[i])) {
+          result.push(array[i])
+        } else {
+          result.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return result.join(' ');
+  }
 }
+
